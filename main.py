da9042ee5fc4b7d2d9f0985a77691b7345956a4a
sambhavm22 <58766591+sambhavm22@users.noreply.github.com>
2023-06-15 10:25:58 +0200

    data ingestion file

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from unittest import result
 from Insurance.logger import logging
 from Insurance.exception import InsuranceException
-import os, sys
 from Insurance.utils import get_collection_as_dataframe
-from Insurance.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig
+import sys, os
+from Insurance.entity.config_entity import DataIngestionConfig
 from Insurance.entity import config_entity
+from Insurance.components.data_ingestion import DataIngestion
 
 # def test_logger_and_exception():
 #     try:
@@ -17,14 +17,18 @@ from Insurance.entity import config_entity
 #         logging.debug(str(e))
 #         raise InsuranceException(e, sys)
     
-if __name__ == '__main__':
+if __name__=="__main__":
     try:
-            #test_logger_and_exception()
-        #get_collection_as_dataframe(database_name="INSURANCE", collection_name='INSURANCE_PROJECT')
-        training_pipeline_config = config_entity.TrainingPipelineConfig()
-        data_ingestion_config = config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
-        print(data_ingestion_config.to_dict())
+          #start_training_pipeline()
+          #test_logger_and_expection()
+       # get_collection_as_dataframe(database_name ="INSURANCE", collection_name = 'INSURANCE_PROJECT')
+       training_pipeline_config = config_entity.TrainingPipelineConfig()
+       
+      #data ingestion
+       data_ingestion_config  = config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+       print(data_ingestion_config.to_dict())
+       data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
+       data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
 
     except Exception as e:
-        print(e)    
-    
+        print(e)
