1a5e39ce97e6433879a8ec96d69816c8e80d7eae
sambhavm22 <58766591+sambhavm22@users.noreply.github.com>
2023-06-15 17:38:46 +0200

    data validation

--- Insurance/entity/config_entity.py
+++ Insurance/entity/config_entity.py
@@ -35,8 +35,11 @@ class DataIngestionConfig:
         try:
             return self.__dict__
         except Exception as e:
-            raise InsuranceException(e, sys) 
+            raise InsuranceException(e, sys)
 
-
-
-
+class DataValidationConfig:
+    def __init__(self, training_pipeline_config:TrainingPipelineConfig):
+        self.data_validation_dir = os.path.join(training_pipeline_config.artifact_dir, "data_validation")
+        self.report_file_path = os.path.join(self.data_validation_dir, "report.yaml") #report can be in yml, json, csv form
+        self.missing_threshold:float = 0.2
+        self.base_file_path = os.path.join('/Users/aakanksha/My_Codes/Project-EWB/artifact/06152023__102240/data_ingestion/feature_store/insurance.csv')
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from Insurance.components import data_validation
 from Insurance.logger import logging
 from Insurance.exception import InsuranceException
 from Insurance.utils import get_collection_as_dataframe
@@ -5,6 +6,7 @@ import sys, os
 from Insurance.entity.config_entity import DataIngestionConfig
 from Insurance.entity import config_entity
 from Insurance.components.data_ingestion import DataIngestion
+from Insurance.components.data_validation import DataValidation
 
 # def test_logger_and_exception():
 #     try:
@@ -29,6 +31,13 @@ if __name__=="__main__":
        print(data_ingestion_config.to_dict())
        data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
        data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+
+      #data validation
+       data_validation_config = config_entity.DataValidationConfig(training_pipeline_config=training_pipeline_config)
+       data_validation = DataValidation(data_validation_config=data_validation_config,
+                         data_ingestion_artifact=data_ingestion_artifact)
+        
+       data_validation_artifact = data_validation.initiate_data_validation()
 
     except Exception as e:
         print(e)
