1051dfcda08bdd80dfbc7d2ed0e609fce299f5ee
sambhavm22 <58766591+sambhavm22@users.noreply.github.com>
2023-06-16 13:12:22 +0200

    model pusher and predictor file

--- Insurance/predictor.py
+++ Insurance/predictor.py
@@ -1,114 +1,94 @@
-import os, sys
+import os
+from Insurance.entity.config_entity import TRANSFORMER_OBJECT_FILE_NAME,MODEL_FILE_NAME,TARGET_ENCODER_OBJECT_FILE_NAME
+from glob import glob
 from typing import Optional
-from Insurance.entity.config_entity import MODEL_FILE_NAME, TRANSFORM_OBJECT_FILE_NAME, TARGET_ENCODER_OBJECT_FILE_NAME
-from Insurance.exception import InsuranceException
+import os
+
+# Now lets start model validation
+
 
 class ModelResolver:
+    
+    def __init__(self,model_registry:str = "saved_models",
+                transformer_dir_name="transformer",
+                target_encoder_dir_name = "target_encoder",
+                model_dir_name = "model"):
 
-    def __init__(self, model_registry:str = "saved_models",
-                 transform_dir_name = "transform", 
-                 target_encoder_dir_name = 'target_encoder',
-                 model_dir_name = "model"):
-        
-        self.model_registry = model_registry
-        os.makedirs(self.model_registry, exist_ok=True)
-        self.transformer_dir_name = target_encoder_dir_name
-        self.target_encoder_dir_name = target_encoder_dir_name
-        self.model_dir_name = model_dir_name
-#1        
+        self.model_registry=model_registry
+        os.makedirs(self.model_registry,exist_ok=True)
+        self.transformer_dir_name = transformer_dir_name
+        self.target_encoder_dir_name=target_encoder_dir_name
+        self.model_dir_name=model_dir_name
+
+# 1
     def get_latest_dir_path(self)->Optional[str]:
         try:
-
-            dir_name = os.listdir(self.model_registry)
-
-            if len(dir_name) == 0:
+            dir_names = os.listdir(self.model_registry)
+            if len(dir_names)==0:
                 return None
-            
-            dir_name = list(map(int, dir_name))
-            latest_dir_name = max(dir_name)
-            return os.path.join(self.model_registry, f"{latest_dir_name}")
-
+            dir_names = list(map(int,dir_names))
+            latest_dir_name = max(dir_names)
+            return os.path.join(self.model_registry,f"{latest_dir_name}")
         except Exception as e:
-            raise InsuranceException(e, sys)
-        
-#2
+            raise e
+
+# 2
+
     def get_latest_model_path(self):
         try:
             latest_dir = self.get_latest_dir_path()
             if latest_dir is None:
-                raise Exception(f"Model is Not available")
-            
-            return os.path.join(latest_dir, self.model_dir_name, MODEL_FILE_NAME)
-
+                raise Exception(f"Model is not available")
+            return os.path.join(latest_dir,self.model_dir_name,MODEL_FILE_NAME)
         except Exception as e:
-            raise InsuranceException(e, sys)
-
-#3
+            raise e
+# 3
     def get_latest_transformer_path(self):
         try:
-
             latest_dir = self.get_latest_dir_path()
             if latest_dir is None:
-                raise Exception(f"Transform data is not available")
-            return os.path.join(latest_dir, self.transformer_dir_name, TRANSFORM_OBJECT_FILE_NAME)
-            
+                raise Exception(f"Transformer is not available")
+            return os.path.join(latest_dir,self.transformer_dir_name,TRANSFORMER_OBJECT_FILE_NAME)
         except Exception as e:
-            raise InsuranceException(e, sys)
-#4
+            raise e
+# 4
     def get_latest_target_encoder_path(self):
         try:
-
             latest_dir = self.get_latest_dir_path()
             if latest_dir is None:
-                raise Exception(f"Target encoder data is not available")
-            return os.path.join(latest_dir, self.target_encoder_dir_name, TARGET_ENCODER_OBJECT_FILE_NAME)
-            
+                raise Exception(f"Target encoder is not available")
+            return os.path.join(latest_dir,self.target_encoder_dir_name,TARGET_ENCODER_OBJECT_FILE_NAME)
         except Exception as e:
-            raise InsuranceException(e, sys)
-        
-#5
-    def get_latest_save_dir_path(self) -> str:
+            raise e
 
+# 5
+    def get_latest_save_dir_path(self)->str:
         try:
-            latest_dir = self.get_latest_model_path()
-            if latest_dir is None:
-                return os.path.join(self.model_registry, f"{0}")
-            
-            latest_dir_num = int(os.path.basename(self.get_latest_save_dir_path()))
-            return os.path.join(self.model_registry, f"{latest_dir_num+1}") #add 1 so that it will increase everytime new folder
-        
+            latest_dir = self.get_latest_dir_path()
+            if latest_dir==None:
+                return os.path.join(self.model_registry,f"{0}")
+            latest_dir_num = int(os.path.basename(self.get_latest_dir_path()))
+            return os.path.join(self.model_registry,f"{latest_dir_num+1}")
         except Exception as e:
-            raise InsuranceException(e, sys)
-        
-
+            raise e
+# 6
     def get_latest_save_model_path(self):
-
         try:
-
-            latest_dir = self.get_latest_model_path()
-            return os.path.join(latest_dir, self.model_dir_name, MODEL_FILE_NAME) #model.pkl
-            
+            latest_dir = self.get_latest_save_dir_path()
+            return os.path.join(latest_dir,self.model_dir_name,MODEL_FILE_NAME)
         except Exception as e:
-            raise InsuranceException(e, sys)    
-        
-
+            raise e
+# 7
     def get_latest_save_transformer_path(self):
         try:
             latest_dir = self.get_latest_save_dir_path()
-            return os.path.join(latest_dir, self.transformer_dir_name, TRANSFORM_OBJECT_FILE_NAME) #transform.pkl
-
+            return os.path.join(latest_dir,self.transformer_dir_name,TRANSFORMER_OBJECT_FILE_NAME)
         except Exception as e:
-            raise e    
-        
-
+            raise e
+# 8
     def get_latest_save_target_encoder_path(self):
         try:
             latest_dir = self.get_latest_save_dir_path()
-            return os.path.join(latest_dir, self.target_encoder_dir_name, TARGET_ENCODER_OBJECT_FILE_NAME) #target_encoder.pkl
-
+            return os.path.join(latest_dir,self.target_encoder_dir_name,TARGET_ENCODER_OBJECT_FILE_NAME)
         except Exception as e:
-            raise e    
-                
-
-
-        
+            raise e
